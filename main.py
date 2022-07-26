88831136c1677c8bb7cd4471d90aca8aa105f708
FawkesMc <mypp32@gmail.com>
2022-07-26 19:01:41 +0700

    Initial commit

--- main.py
+++ main.py
@@ -2,6 +2,5 @@ from UI import UI
 import logging
 
 if __name__ == "__main__":
-    logging.basicConfig(filename='log.txt', filemode="w", encoding="utf-8",
-                        level=logging.DEBUG, format='%(asctime)s %(message)s')
+    #logging.basicConfig(filename='log.txt', filemode="w", encoding="utf-8", level=logging.DEBUG, format='%(asctime)s %(message)s')
     UI()
